0de7b7a3666c325e3497e57e8d4040c0458885ab
Priyanshu Kumar <priyanshukm@uber.com>
2019-11-19 14:03:15 +0530

    Added mutiprocessing to reduce time from 200 sec to 15 sec

--- script.py
+++ script.py
@@ -1,8 +1,12 @@
 from phabricator import Phabricator
 from collections import OrderedDict
+from multiprocessing import Process
+from multiprocessing import Manager
+import os
 import sys
 
 # TODO: Too slow, add mutiprocessing [~~ 2 min 30 sec] for single user
+# Time reduced to [~~ 13-15 sec]
 
 def getUserPhid(user):
   result = phab.user.query(usernames=[user])
@@ -29,8 +33,20 @@ def getLatestDiffID(diffs):
   keys = list(sortedDiffs.keys())
   return keys[0]
 
-totalplus = 0
-totalminus = 0
+# def process_info(title):
+#   print(title)
+#   print('module name:', __name__)
+#   print('parent process:', os.getppid())
+#   print('process id:', os.getpid())
+
+def processLatestDiff(rev, shared_dict):
+  # process_info(rev)
+  allDiffs = getDiffs([rev])                #Get all diffs from current revision
+  latestDiffID = getLatestDiffID(allDiffs)  #Get latest diff from current revision
+  for changedFile in allDiffs[str(latestDiffID)]["changes"]:
+    shared_dict["addLines"] = shared_dict["addLines"] + int(changedFile["addLines"])
+    shared_dict["delLines"] = shared_dict["delLines"] + int(changedFile["delLines"])
+
 phab = Phabricator() #Uses config from local ~/.arcrc file
 phab.update_interfaces()
 
@@ -38,15 +54,21 @@ username = sys.argv[1]
 revisions = getUserRevisionList(username)
 diffcount = len(revisions)
 
+processes = []
+manager = Manager()
+shared_dict = manager.dict()
+shared_dict["addLines"] = 0
+shared_dict["delLines"] = 0
+
 for rev in revisions:
-  diffs = getDiffs([rev])
-  # Get latest diff from given revision
-  latestDiff = getLatestDiffID(diffs)
-  for fil in diffs[str(latestDiff)]["changes"]:
-    totalplus = totalplus + int(fil["addLines"])
-    totalminus = totalminus + int(fil["delLines"])
+  p = Process(target = processLatestDiff, args = (rev, shared_dict))
+  p.start()
+  processes.append(p)
+
+for p in processes:
+  p.join()
 
 print("Total Diffs Raised: " + str(diffcount))
-print("Total Lines Added: " + str(totalplus))
-print("Total Lines Removed: " + str(totalminus))
-print("Total Lines: " + str(totalplus + totalminus))
+print("Total Lines Added: " + str(shared_dict["addLines"]))
+print("Total Lines Removed: " + str(shared_dict["delLines"]))
+print("Total Lines: " + str(shared_dict["addLines"] + shared_dict["delLines"]))
